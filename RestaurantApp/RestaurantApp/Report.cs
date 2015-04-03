c9de0d7a95472af0f2bd0f68c5841af1ed14776b
Ekaterina Martynyuk <martynyuk88@gmail.com>
2015-04-03 16:31:02 +0300

    Added Report.cs

---
+++ RestaurantApp/RestaurantApp/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp
+{
+    public static class Report
+    {
+        private static Dictionary<Table, AuthorizedEmployee> reportsFromTables = new Dictionary<Table, AuthorizedEmployee>();
+
+        public static Dictionary<Table, AuthorizedEmployee> ReportsFromTables
+        {
+            get { return reportsFromTables; }
+            private set { reportsFromTables = value; }
+        }
+    }
+}
