6ce83c3107506c71773d88b48b27cb75caaf2392
vot24100 <mladen.m.savov@gmail.com>
2015-04-05 18:35:35 +0300

    Added custom exception class - RestaurantException.cs

---
+++ RestaurantApp/RestaurantApp/RestaurantException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp
+{
+    class RestaurantException : ApplicationException
+    {
+        public RestaurantException(string message) 
+            : base(message)
+        {
+        }
+
+        public RestaurantException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
