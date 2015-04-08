43cb2128ef23e8239c1c31e99ee11ca963fc39f0
Nikolay Pavlov <n.pavlov10@yahoo.com>
2015-04-08 12:21:49 +0300

    Added DishesToCook.cs and fixed references

---
+++ RestaurantApp/RestaurantApp/Persons/Personnel/DishesToCook.cs
@@ -0,0 +1,27 @@
+﻿namespace RestaurantApp.Persons.Personnel
+{
+    using System.Collections;
+
+    using RestaurantApp.Engine;
+
+    public delegate void MyDelegate(MenuItem item);
+
+    public class DishesToCook : ArrayList
+    {
+        public event MyDelegate MyEvent;
+
+        protected virtual void OnChanged(MenuItem item)
+        {
+            if (MyEvent != null)
+            {
+                MyEvent(item);
+            }
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            Add(item);
+            OnChanged(item);
+        }
+    }
+}
